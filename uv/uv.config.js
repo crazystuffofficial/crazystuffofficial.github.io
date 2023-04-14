c1d3c936ae03a243006f1eef0b52fccbb8bd260b
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-04-14 06:38:00 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/service/",
-  bare: "https://tiwmain.tiwweb.workers.dev/",
+  bare: "https://b99bb8bbd2bd6258262f1715ba276f58.cloudflareworkers.com/",
   encodeUrl: Ultraviolet.codec.base64.encode,
   decodeUrl: Ultraviolet.codec.base64.decode,
   handler: "/uv/uv.handler.js",
